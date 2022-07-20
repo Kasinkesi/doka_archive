9eec65d566ec5e8c3faf0ec44981f0d39e6a75d1
Doka <razdolbay89@gmail.com>
2022-07-20 16:32:21 +0300

    rle codec begins

---
+++ lib/codecs/rle/rle.py
@@ -0,0 +1,32 @@
+def compact(line, cnt_max=255):
+    if not line:
+        return ''
+
+    i, cur, count = 1, line[0:1], 1
+    res = b''
+
+    while i <= len(line):
+        s = line[i:i + 1]
+        if s == cur:
+            if count == cnt_max:
+                res += str(count).encode() + cur
+                count = 0
+            count += 1
+        else:
+            res += str(count).encode() + cur
+            cur, count = s, 1
+        i += 1
+    return res
+
+
+if __name__ == '__main__':
+    print(compact(''))
+    print(compact(('A' * 700 + 'B' * 700 + 'Б').encode()))
+    print(compact('AA'.encode()))
+    print(compact('AAB'.encode()))
+    print(compact('Б'.encode()))
+    print(compact('ББ'.encode()))
+    print(compact('ББЮ'.encode()))
+    print(compact(b'AABAAA'))
+    print(compact(b'ABC'))
+    print(compact(b'AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB'))
