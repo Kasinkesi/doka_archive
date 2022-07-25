553889b7656b35737b8fee639a24dd07d883f04b
Doka <razdolbay89@gmail.com>
2022-07-25 12:55:14 +0300

    divide class Codec and class Rle

--- lib/codecs/rle/main.py
+++ lib/codecs/rle/main.py
@@ -1,78 +1,123 @@
-class Rle:
-    def __init__(self, line_length=1000):
-        self.line_length = line_length
+import io, sys
+
+
+class DecodeError(Exception):
+    pass
+
+
+class EOFError(DecodeError):
+    pass
+
+
+class MalformedDataError(DecodeError):
+    pass
+
+
+class MagicMismatchError(MalformedDataError):
+    pass
+
+
+class Codec:
+    def compress_stream(self, instream, outstream):
+        raise NotImplementedError()
+
+    def decompress_stream(self, instream, outstream):
+        raise NotImplementedError()
+
+    @property
+    def magic(self):
+        raise NotImplementedError()
+
+    def compress(self, data):
+        res = io.BytesIO()
+        self.compress_stream(io.BytesIO(data), res)
+        return res.getvalue()
+
+    def decompress(self, data):
+        res = io.BytesIO()
+        self.decompress_stream(io.BytesIO(data), res)
+        return res.getvalue()
+
+    def compress_file(self, src, dst):
+        with open(src, 'rb') as srcfile, open(dst, 'wb') as dstfile:
+            dstfile.write(self.magic)
+            self.compress_stream(srcfile, dstfile)
+
+    def decompress_file(self, src, dst):
+        with open(src, 'rb') as srcfile:
+            header = srcfile.read(len(self.magic))
+            if header != self.magic:
+                raise MagicMismatchError("Got {} while {} was expected".format(header, self.magic))
+
+            with open(dst, 'wb') as dstfile:
+                self.decompress_stream(srcfile, dstfile)
+
+
+class Rle(Codec):
+    def __init__(self, some_parametr=1000):
+        self.some_parametr = some_parametr
         self.count = None
 
-    def compres(self, array):
-        if not array:
+    @property
+    def magic(self):
+        return b"rle"
+
+    def compress_stream(self, instream, outstream):
+        line = instream.read()
+        if not line:
             return b''
 
-        cur, count = array[0], 1
-        comp_array = bytearray()
+        cur, count = line[0], 1
+        res = bytearray()
 
-        for s in list(array[1:]) + [None]:
+        for s in list(line[1:]) + [None]:
             if s == cur:
                 if count == 255:
-                    comp_array.append(count)
-                    comp_array.append(cur)
+                    res.append(count)
+                    res.append(cur)
                     count = 0
                 count += 1
             else:
-                comp_array.append(count)
-                comp_array.append(cur)
+                res.append(count)
+                res.append(cur)
                 cur, count = s, 1
-        return comp_array
+        outstream.write(res)
 
-    def decompres(self, byte_line):
+    def decompress_stream(self, instream, outstream):
+        comp_line = instream.read()
         res = bytearray()
-        for s in byte_line:
+        for s in comp_line:
             if not self.count:
                 self.count = s
             else:
-                res.extend([s]*self.count)
+                res.extend([s] * self.count)
                 self.count = None
-        return res
-
-    def archive(self, input, output):
-        input = open(input, 'rb')
-        output = open(output, 'wb')
-        while True:
-            line = input.read(self.line_length)
-            if not line:
-                break
-            comp_line = self.compres(line)
-            output.write(comp_line)
-        input.close()
-        output.close()
-
-    def dearchive(self, input, output):
-        '''must be even line length'''
-        input = open(input, 'rb')
-        output = open(output, 'wb')
-        line = True
-        while line:
-            line = input.read(100)
-            comp_line = self.decompres(line)
-            output.write(comp_line)
-        input.close()
-        output.close()
+        outstream.write(res)
 
 
 if __name__ == '__main__':
     rle = Rle()
-    print(rle.decompres(rle.compres('')))
-    print(rle.compres(('A' * 700 + 'B' * 700 + 'Б').encode()))
-    print(rle.decompres(rle.compres(('A' * 700 + 'B' * 700 + 'Б').encode())))
-    print(rle.decompres(rle.compres('AA'.encode())))
-    print(rle.decompres(rle.compres('AAB'.encode())))
-    print(rle.decompres(rle.compres('1'.encode())))
-    print(rle.decompres(rle.compres('11'.encode())))
-    print(rle.decompres(rle.compres('112'.encode())))
-    print(rle.decompres(rle.compres('0044'.encode())))
-    print(rle.decompres(rle.compres(('4' * 202).encode())))
-    print(rle.decompres(rle.compres('Б'.encode())))
-    print(rle.decompres(rle.compres('ББ'.encode())))
-    print(rle.decompres(rle.compres('ББЮ'.encode())))
-    print(rle.decompres(rle.compres(b'AABAAA')))
-    print(rle.decompres(rle.compres(b'ABC')))
-    print(rle.decompres(rle.compres(b'AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')))
+    print(rle.decompress(rle.compress(b'')))
+    rle.compress(('A' * 700 + 'B' * 700 + 'Б').encode())
+    print(rle.decompress(rle.compress(('A' * 700 + 'B' * 700 + 'Б').encode())))
+    print(rle.decompress(rle.compress('AA'.encode())))
+    print(rle.decompress(rle.compress('AAB'.encode())))
+    print(rle.decompress(rle.compress('0044'.encode())))
+    print(rle.decompress(rle.compress(('4' * 202).encode())))
+    print(rle.decompress(rle.compress('Б'.encode())))
+    print(rle.decompress(rle.compress('ББ'.encode())))
+    print(rle.decompress(rle.compress('ББЮ'.encode())))
+    print(rle.decompress(rle.compress(b'AABAAA')))
+    print(rle.decompress(rle.compress(b'ABC')))
+    print(rle.decompress(rle.compress(b'AAAABBBCCXYZDDDDEEEFFFAAAAAABBBBBBBBBBBBBBBBBBBBBBBBBBBB')))
+    rle.compress_file('filetest.txt', 'bytefile.txt')
+    rle.decompress_file('bytefile.txt', 'control.txt')
+    rle.compress_file('test.png', 'bytefile_png.txt')
+    rle.decompress_file('bytefile_png.txt', 'control.png')
+    rle.compress_file('Noize MC - Работа.mp3', 'bytefile_mp3.txt')
+    rle.decompress_file('bytefile_mp3.txt', 'control.mp3')
+    try:
+        rle.decompress_file('broken_bytefile.txt', 'broken_control.txt')
+    except MagicMismatchError:
+        print(sys.exc_info()[0:2])
+
